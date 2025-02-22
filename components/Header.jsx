94c79720f50d76a509da091bc2fce0415a6bda4a
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-22 17:40:47 +0100

    fix:made each new text input appear on the screen not replace the previous text and added a clear chat button

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,18 @@
+import { usetextContext } from '@/context/TextInputContext'
 import React from 'react'
 
 const Header = () => {
+  const { text, setText,setTranslateTo} = usetextContext()
+
+  const clearChat =()=>{
+    setText([]),
+    setTranslateTo([])
+  }
+  
   return (
     <header className='flex justify-between items-center p-5 text-xl'>
       <p className="">AI Text Processor</p>
-      <p className=""></p>
+      <button onClick={clearChat} className={`${text.length === 0 ? 'hidden' : ''} w-fit p-4 border-2 border-[#262b47] hover:bg-[#262b47] hover:text-white rounded-2xl cursor-pointer`}> Clear chat</button>
     </header>
   )
 }
--- components/TextInput.jsx
+++ components/TextInput.jsx
@@ -1,18 +1,27 @@
-import { textContext, usetextContext } from '@/context/TextInputContext'
+import { usetextContext } from '@/context/TextInputContext'
 import SendIcon from '@/icons/Send'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import TranslateButton from './TranslateButton'
 
 const TextInput = () => {
   const [getText, setGetText ] = useState('')
-  const {text, setText,setTranslation} = usetextContext()
+  const {text, setText} = usetextContext()
 
-  useEffect(()=>{
-
-  })
   const submitText =(e)=>{
-    setText(getText)
+    localStorage.setItem("visited", true);
+
+    setText((prev) => [
+      ...prev,
+      {
+        text: getText,
+        language: "",
+        percentage: 0,
+        translation: "",
+        translationErr: null,
+        notSupported: null
+      }
+    ])
     setGetText('')
-    setTranslation('')
     e.preventDefault();
   }
 
@@ -20,7 +29,13 @@ const TextInput = () => {
 
   return (
     <div className='absolute bottom-10 w-full'>
-        <form className="relative lg:mx-10 ">
+      {text.length > 0 && 
+      <div className='flex justify-self-end md:mr-5 lg:mr-24 gap-5'>
+      <TranslateButton className=''/>
+      <button className="w-fit p-4 border-2 border-[#262b47] group hover:bg-[#6b76b4] hover:cursor-not-allowed hover:text-white rounded-2xl overflow-hidden flex items-center gap-4 cursor-pointer">Summarize</button>
+      </div>
+      }
+        <form className="relative lg:mx-10 mt-2 ">
             <input value={getText} onChange={(e) => setGetText(e.target.value)} placeholder="Enter text here" className='w-full border-2 rounded-3xl focus:outline-none hover:border-[#02040F] p-4  pl-5 pr-16'/>
             <button onClick={submitText} ><SendIcon className='absolute top-3 right-5 hover:scale-105'/></button>
         </form>

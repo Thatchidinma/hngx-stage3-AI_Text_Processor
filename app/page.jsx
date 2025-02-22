e1a47a69ac45c886096dcfcddc8789c5858bbe6e
Ndukuba Nneoma Chidinma <ndukubachidinma@gmail.com>
2025-02-22 10:22:21 +0100

    feat: removed summarize button

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,7 @@ export default function Home() {
         <>
         <p className="p-4 border-r-8 rounded-3xl rounded-bl max-w-[80%] lg:max-w-[50%] border">Detected {percentage} {language}</p>
         <TranslateButton/>
-        <button className="w-fit p-4 border-2 border-[#262b47] group hover:bg-[#262b47] hover:text-white rounded-2xl overflow-hidden flex items-center gap-4 cursor-pointer">Summarize</button>
+        {/* <button className="w-fit p-4 border-2 border-[#262b47] group hover:bg-[#262b47] hover:text-white rounded-2xl overflow-hidden flex items-center gap-4 cursor-pointer">Summarize</button> */}
         {translation && <div className="p-4 border-l-8 rounded-3xl rounded-br max-w-[80%] lg:max-w-[50%] border ml-auto ">
           {translation &&
           <>
